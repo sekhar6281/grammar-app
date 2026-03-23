d2e07514a1a9714a16153121b2f61b71ec3e292d
sekhar <sekhar3342.gmail.com>
2026-03-23 12:37:12 +0530

    changed reqirements file and render build file

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,11 @@
+cat <<EOF > render-build.sh
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
+
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# This runs the database initialization
 python app.py
+EOF
